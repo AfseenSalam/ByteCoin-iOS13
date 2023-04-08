639b8fb57a61e0ea68f52c2aa96f63fcd5a115fe
AfseenSalam <afseenfazil@gmail.com>
2023-04-08 18:19:00 -0400

    Adding Dividor

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
+
+//Dividor
 //MARK: - CoinManagerDelegate
     
     extension ViewController : CoinManagerDelegate{
